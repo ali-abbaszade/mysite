8507cc18dcd81b76beacef6ebafebe0d39a54493
ali-abbaszade <abbasszade.a@gmail.com>
2022-04-21 13:46:18 +0430

    created admin pages for blog and contact

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('title', 'counted_views', 'status',
+                    'published_date', 'created_date')
+    # fields = ('title',)
+    # exclude = ('status',)
+    # ordering = ['-created_date']
+    list_filter = ('status',)
+    search_fields = ['title', 'content']
+
+
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from tabnanny import verbose
 from django.db import models
 
 
@@ -13,3 +14,9 @@ class Post(models.Model):
     published_date = models.DateTimeField(null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return self.title
