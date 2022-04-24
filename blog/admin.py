c63c7000631a4fe6a2647cb5ada84f610cbe94f6
ali-abbaszade <abbasszade.a@gmail.com>
2022-04-24 13:23:26 +0430

    updated blog

--- blog/admin.py
+++ blog/admin.py
@@ -7,7 +7,7 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = '-empty-'
-    list_display = ('title', 'counted_views', 'status',
+    list_display = ('id', 'title', 'counted_views', 'status',
                     'published_date', 'created_date')
     # fields = ('title',)
     # exclude = ('status',)
