25bc1ed0e01f1779906e2c6ab029b52b7993c65f
ali-abbaszade <abbasszade.a@gmail.com>
2022-05-17 21:02:16 +0430

    updated blog-single and blog-home

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from blog.models import Post
 from django.utils import timezone
 
 
+
 def blog_view(request):
     now = timezone.now()
     posts = Post.objects.filter(status=1, published_date__lte=now)
@@ -12,8 +13,14 @@ def blog_view(request):
 
 def blog_single(request, pid):
     post = get_object_or_404(Post, pk=pid, status=1)
+
+    single = Post.objects.get(id=pid)
+    next_post = Post.objects.filter(id__gt=single.id).order_by('id').first()
+    prev_post = Post.objects.filter(id__lt=single.id).order_by('id').last()
+
     post.counted_views = post.counted_views + 1
-    context = {'post': post}
+
+    context = {'post': post, 'single' : single, 'next_post':next_post, 'prev_post':prev_post}
     post.save()
     return render(request, 'blog/blog-single.html', context)
 
