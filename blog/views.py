c82ccab957f9d7ca48547cdfc05d672cf705e30d
ali-abbaszade <abbasszade.a@gmail.com>
2022-05-25 12:53:00 +0430

    added search filter

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,7 @@ from django.utils import timezone
 def blog_view(request, **kwargs):
     now = timezone.now()
     posts = Post.objects.filter(status=1, published_date__lte=now)
-    print(kwargs)
+    
     if kwargs.get('cat_name'):
         posts = posts.filter(category__name=kwargs['cat_name'])
 
@@ -36,6 +36,16 @@ def blog_single(request, pid):
 def blog_category(request, cat_name):
     posts = Post.objects.filter(status=1)
     posts = Post.objects.filter(category__name=cat_name)
+    context = {'posts':posts}
+    return render(request, 'blog/blog-home.html', context)
+
+def blog_search(request):
+    posts = Post.objects.filter(status=1)
+    if request.method == 'GET':
+        # print(request.GET.get('s'))
+        if s := request.GET.get('s'):
+            posts = posts.filter(content__contains=s)
+
     context = {'posts':posts}
     return render(request, 'blog/blog-home.html', context)
 
