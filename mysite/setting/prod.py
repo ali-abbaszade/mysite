44d00b4c36d99a05372da62523be20c054c63f99
ali-abbaszade <abbasszade.a@gmail.com>
2022-07-07 01:38:49 +0430

    added security config

--- mysite/setting/prod.py
+++ mysite/setting/prod.py
@@ -35,3 +35,27 @@ MEDIA_ROOT = BASE_DIR / 'media'
 STATICFILES_DIRS = [
     BASE_DIR / "statics",
 ]
+
+# X-Frame-Options
+X_FRAME_OPTIONS = 'DENY'
+
+#X-Content-Type-Options
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+# Strict-Transport-Security
+SECURE_HSTS_SECONDS = 15768000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+# that requests over HTTP are redirected to HTTPS
+SECURE_SSL_REDIRECT = True 
+
+# X-XSS-Protection
+SECURE_BROWSER_XSS_FILTER = True
+
+# for more security
+CSRF_COOKIE_SECURE = True
+CSRF_USE_SESSIONS = True
+CSRF_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_SAMESITE = 'Strict'
